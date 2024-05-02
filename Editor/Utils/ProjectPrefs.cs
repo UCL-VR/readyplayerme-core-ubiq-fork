82c67f3c28b3775b12cfb9a64af7a7fee9e9ce2d
bnco <email>
2024-05-02 12:49:37 +0100

    Fix for inconsistent pref key usage in readyplayerme avatarloader

--- Editor/Utils/ProjectPrefs.cs
+++ Editor/Utils/ProjectPrefs.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public static class ProjectPrefs
 {
+    // Two pref keys as the loader package sometimes uses the alt key 
     public const string FIRST_TIME_SETUP_DONE = "first-time-setup-guide";
+    public const string FIRST_TIME_SETUP_DONE_ALT = "first-time-setup"; 
 
     public static bool GetBool(string key)
     {
-        if (key == FIRST_TIME_SETUP_DONE)
+        if (key == FIRST_TIME_SETUP_DONE || key == FIRST_TIME_SETUP_DONE_ALT)
         {
             return true;
         }
